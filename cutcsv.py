76b1e9c1d51e30aabdbc03e4ceaf1c2f5945f965
Karl Lin <linroex@coder.tw>
2016-08-03 16:38:13 +0800

    add page size

--- cutcsv.py
+++ cutcsv.py
@@ -9,9 +9,13 @@ def main():
     target_file_path = argv[1]
     decision_column_index = int(argv[2])
 
+    output_files = {}
+
     with open(target_file_path, 'r', encoding='utf8') as f:
         keys = []
         output_buffer = {}
+
+        max_page_size = 1000
 
         for line in f:
             key = line.split(',')[decision_column_index]
@@ -19,19 +23,23 @@ def main():
                 output_buffer[key].append(line)
             else:
                 keys.append(key)
+                output_files[key] = open(base_path + '/' + key + '.csv', 'w', encoding='utf8')
                 output_buffer[key] = [line]
 
+            if len(output_buffer[key]) >= max_page_size:
+                output_files[key].write('\n'.join(output_buffer[key]))
+                output_buffer[key] = []
+
     for key in keys:
-        with open(base_path + '/' + key + '.csv', 'w', encoding='utf8') as f:
-            f.write('\n'.join(output_buffer[key]))
+        output_files[key].close()
 
 if __name__ == '__main__':
 
     base_path = path.dirname(path.realpath(__file__))
 
-    # start = datetime.now()
-    # main()
-    # print(datetime.now() - start)
+    start = datetime.now()
+    main()
+    print(datetime.now() - start)
 
     # print(timeit.timeit(main, number=6))
-    cProfile.run('main()')
+    # cProfile.run('main()')
